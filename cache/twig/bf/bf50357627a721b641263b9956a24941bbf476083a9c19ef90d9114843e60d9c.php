b6ed3aabd5486a8d44e073053944b9f716b07892
Jerry <g.gravitas@gmail.com>
2018-08-10 14:45:07 -0400

    tinkering

---
+++ cache/twig/bf/bf50357627a721b641263b9956a24941bbf476083a9c19ef90d9114843e60d9c.php
@@ -0,0 +1,46 @@
+<?php
+
+/* partials/scripts.html.twig */
+class __TwigTemplate_ff1d6edb693d0ddb34dc6578765b6f202ef30c33520888cf8e1a32fed3fc4978 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!--- Your global JavaScript code can go here -->
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "partials/scripts.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<!--- Your global JavaScript code can go here -->
+", "partials/scripts.html.twig", "/Users/temp/repos/grav/user/themes/quark-open-publishing/templates/partials/scripts.html.twig");
+    }
+}
